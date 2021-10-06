f5c57946e1f69175da1ade9c948568356737824f
jummi <98qkrwjdal@naver.com>
2021-10-06 22:26:13 +0900

    Initial commit

---
+++ src/main/java/org/prgrms/kdtjpa/KdtJpaApplication.java
@@ -0,0 +1,13 @@
+package org.prgrms.kdtjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KdtJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KdtJpaApplication.class, args);
+    }
+
+}
