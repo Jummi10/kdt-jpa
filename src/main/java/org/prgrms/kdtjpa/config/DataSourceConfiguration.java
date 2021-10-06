fa5f293e2f3790bf50ffcc634dfb1538c77d311a
jummi <98qkrwjdal@naver.com>
2021-10-06 22:34:47 +0900

    chore: db 연결 설정 (W8D1)

---
+++ src/main/java/org/prgrms/kdtjpa/config/DataSourceConfiguration.java
@@ -0,0 +1,70 @@
+package org.prgrms.kdtjpa.config;
+
+import java.util.Properties;
+
+import javax.sql.DataSource;
+
+import org.springframework.boot.autoconfigure.orm.jpa.JpaProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.AbstractJpaVendorAdapter;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+
+@Configuration
+public class DataSourceConfiguration {
+
+    @Bean
+    public DataSource dataSource() {
+        // 어떤 database를 사용할 것인지
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("org.h2.Driver");
+        dataSource.setUrl("jdbc:h2:~/order-test");
+        dataSource.setUsername("sa");
+        dataSource.setPassword("");
+
+        return dataSource;
+    }
+
+    @Bean
+    public JpaVendorAdapter jpaVendorAdapter(JpaProperties jpaProperties) { // application.yml의 jpa property
+        // JPA interface의 구현체 결정
+        AbstractJpaVendorAdapter jpaVendorAdapter = new HibernateJpaVendorAdapter();
+        jpaVendorAdapter.setShowSql(jpaProperties.isShowSql());
+        jpaVendorAdapter.setDatabasePlatform(jpaProperties.getDatabasePlatform());
+        jpaVendorAdapter.setGenerateDdl(jpaProperties.isGenerateDdl());
+
+        return jpaVendorAdapter;
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource dataSource,
+        JpaVendorAdapter jpaVendorAdapter, JpaProperties jpaProperties) {
+
+        LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
+        em.setDataSource(dataSource);
+        em.setPackagesToScan("org.prgrms.kdtjpa.domain");   // table과 매핑된 entity를 어떤 패키지에 두고 관리할 것인지
+        em.setJpaVendorAdapter(jpaVendorAdapter);
+
+        Properties properties = new Properties();
+        properties.putAll(jpaProperties.getProperties());
+        em.setJpaProperties(properties);
+
+        return em;
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager(LocalContainerEntityManagerFactoryBean entityManagerFactory) {
+
+        // @Transactional에 동작, 관리
+        JpaTransactionManager transactionManager = new JpaTransactionManager();
+        transactionManager.setEntityManagerFactory(entityManagerFactory.getObject());
+
+        return transactionManager;
+    }
+
+}
